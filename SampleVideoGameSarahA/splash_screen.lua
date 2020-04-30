3a3e22165b717351c3a364a487039cf43930cd93
Sarah Andrew <sarah.andrew@ocsbstudent.ca>
2020-04-30 13:58:23 -0400

    Finished day 23

--- SampleVideoGameSarahA/splash_screen.lua
+++ SampleVideoGameSarahA/splash_screen.lua
@@ -23,22 +23,19 @@ local scene = composer.newScene( sceneName )
 -----------------------------------------------------------------------------------------
  
 -- The local variables for this scene
-local beetleship
+local spaceship
 local scrollXSpeed = 8
 local scrollYSpeed = -3
-local spaceSound = audio.loadSound("Sounds/space.mp3.mp3")
-local spaceSoundChannel
-
 local spaceSound = audio.loadSound("Sounds/spaceMusic.mp3.mp3")
 local spaceSoundChannel
+
 --------------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------
 
 -- The function that moves the beetleship across the screen
-local function moveBeetleship()
-    beetleship.x = beetleship.x + scrollXSpeed
-    beetleship.y = beetleship.y + scrollYSpeed
+local function movespaceship()
+    spaceship.alpha = spaceship.alpha -0.001
 end
 
 -- The function that will go to the main menu 
@@ -60,14 +57,17 @@ function scene:create( event )
     display.setDefault("background", 0, 0, 0)
 
     -- Insert the beetleship image
-    beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+    spaceship = display.newImageRect("Images/spaceship.png", 200, 200)
+
+    --character image to be transparent
+    spaceship.alpha = 1
 
     -- set the initial x and y position of the beetleship
-    beetleship.x = 100
-    beetleship.y = display.contentHeight/2
+    spaceship.x = 100
+    spaceship.y = display.contentHeight/2
 
     -- Insert objects into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( beetleship )
+    sceneGroup:insert( spaceship )
 
 end -- function scene:create( event )
 
@@ -92,10 +92,10 @@ function scene:show( event )
 
     elseif ( phase == "did" ) then
         -- start the splash screen music
-        jungleSoundsChannel = audio.play(jungleSounds )
+       spaceSoundChannel = audio.play( spaceSounds )
 
         -- Call the moveBeetleship function as soon as we enter the frame.
-        Runtime:addEventListener("enterFrame", moveBeetleship)
+        Runtime:addEventListener("enterFrame", movespaceship)
 
         -- Go to the main menu screen after the given time.
         timer.performWithDelay ( 3000, gotoMainMenu)          
